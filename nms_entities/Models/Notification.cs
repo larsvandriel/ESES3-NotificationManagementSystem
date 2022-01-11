fd994a2cc10c49c200be05e4a5672ef03a8d1dfd
larsvandriel <lars.van.driel@hotmail.com>
2022-01-11 09:31:24 +0100

    Created Models for NotificationManagementSystem

---
+++ nms_entities/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationManagementSystem.Entities.Models
+{
+    public abstract class Notification
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public List<Employee> Receivers { get; set; }
+    }
+}
