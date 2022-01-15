6e9d335a444d838994453a2c4f4aa366448e9cc7
larsvandriel <lars.van.driel@hotmail.com>
2022-01-16 00:47:18 +0100

    Added Helper methods

---
+++ nms_entities/ShapedEntities/ShapedEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationManagementSystem.Entities.ShapedEntities
+{
+    public class ShapedEntity
+    {
+        public Guid Id { get; set; }
+        public Entity Entity { get; set; }
+
+        public ShapedEntity()
+        {
+            Entity = new Entity();
+        }
+    }
+}
