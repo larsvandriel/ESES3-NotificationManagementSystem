da9f87379ebe443cd3196ffd00c8eae88ff506f0
larsvandriel <lars.van.driel@hotmail.com>
2022-01-16 02:08:25 +0100

    Setup settings for API

---
+++ nms_api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,78 @@
+﻿using LoggingService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using NotificationManagementSystem.API.Filters;
+using NotificationManagementSystem.Contracts;
+using NotificationManagementSystem.Entities;
+using NotificationManagementSystem.Entities.Helpers;
+using NotificationManagementSystem.Entities.Models;
+using NotificationManagementSystem.Repository;
+
+namespace NotificationManagementSystem.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        public static void ConfigureMsSqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["mssqlconnection:connectionString"];
+            services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<ISortHelper<Notification>, SortHelper<Notification>>();
+            services.AddScoped<IDataShaper<Notification>, DataShaper<Notification>>();
+
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+        }
+
+        public static void RegisterFilters(this IServiceCollection services)
+        {
+            services.AddScoped<ValidateMediaTypeAttribute>();
+        }
+
+        public static void AddCustomMediaTypes(this IServiceCollection services)
+        {
+            services.Configure<MvcOptions>(config =>
+            {
+                var newtonsoftJsonOutputFormatter = config.OutputFormatters.OfType<NewtonsoftJsonOutputFormatter>()?.FirstOrDefault();
+
+                if (newtonsoftJsonOutputFormatter != null)
+                {
+                    newtonsoftJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.eses2.hateoas+json");
+                }
+                var xmlOutputFormatter = config.OutputFormatters.OfType<XmlDataContractSerializerOutputFormatter>()?.FirstOrDefault();
+
+                if (xmlOutputFormatter != null)
+                {
+                    xmlOutputFormatter.SupportedMediaTypes.Add("application/vnd.eses2.hateoas+xml");
+                }
+            });
+        }
+    }
+}
