b10f09f36819150fbf732f49a0cb6e7211729214
larsvandriel <lars.van.driel@hotmail.com>
2022-01-16 02:22:44 +0100

    Added Controller for Notification

--- nms_contracts/INotificationRepository.cs
+++ nms_contracts/INotificationRepository.cs
@@ -1,10 +1,13 @@
-﻿using NotificationManagementSystem.Entities.Models;
+﻿using NotificationManagementSystem.Entities.Helpers;
+using NotificationManagementSystem.Entities.Models;
+using NotificationManagementSystem.Entities.Parameters;
 using NotificationManagementSystem.Entities.ShapedEntities;
 
 namespace NotificationManagementSystem.Contracts
 {
     public interface INotificationRepository: IRepositoryBase<Notification>
     {
+        PagedList<ShapedEntity> GetAllNotifications(NotificationParameters notificationParameters);
         ShapedEntity GetNotificationById(Guid notificationId, string fields);
         Notification GetNotificationById(Guid notificationId);
         void CreateNotification(Notification notification);
